5813ced74a44e470d5c6018b402bf0b34aef5759
Emma-Belg <emma.wishart@live.com.au>
2020-12-03 09:53:33 +0100

    created routing for menu items with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react'
+import {Route, BrowserRouter} from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from "./pages/homepage/homepage.component";
 
+const HatsPage = () =>(
+    <div>
+        <h1>Hats</h1>
+    </div>
+)
+
 function App() {
   return (
     <div>
-      <HomePage />
+        <BrowserRouter>
+            <Route exact path='/' component={HomePage}/>
+            <Route exact path='/hats' component={HatsPage}/>
+        </BrowserRouter>
+
+      {/*<HomePage />*/}
     </div>
   );
 }
